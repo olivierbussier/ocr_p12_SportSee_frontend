e806807304185c4e0cacfcf60101fb8e2640c0fc
Olivier Bussier <olivier@bussier.fr>
2023-02-04 08:48:13 +0100

    Ajout du Routeur

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
-import DashBoard from "./pages/DashBoard";
-import Nav from "./composants/Nav";
+import { Routes, Route } from "react-router-dom";
+
 import Container from "./composants/Container";
+import Nav from "./composants/Nav";
+
+import DashBoard from "./pages/DashBoard";
+import Error from "./pages/Error";
 
 import "./App.css";
 
@@ -8,7 +12,13 @@ function App() {
   return (
     <Container>
       <Nav>
-        <DashBoard />
+        <Routes>
+          <Route path='/'         element={<DashBoard />} />
+          <Route path='/profil'   element={<Error code="Aie" text="La page 'Profil' n'existe pas encore" />} />
+          <Route path='/settings' element={<Error code="Aie" text="La page 'Réglages' n'existe pas encore" />} />
+          <Route path='/social'   element={<Error code="Aie" text="La page 'Communauté' n'existe pas encore" />} />
+          <Route path='*'         element={<Error />} />
+        </Routes>
       </Nav>
     </Container>
   );
--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { BrowserRouter } from "react-router-dom";
+
+import App from "./App";
+
+/**
+ * The BrowserRouter instance is placed here in order to facilitate
+ * the test of App component using Jest
+ * Then, to test with Jest you have to declare a Memory router in Jest test
+ * like this :
+ *
+ *  test("landing on a file page", () => {
+ *  // use <MemoryRouter> when you want to manually control the history
+ *  render(
+ *    <MemoryRouter initialEntries={["<some route>>"]}>
+ *      <App />
+ *    </MemoryRouter>
+ *  );
+ *
+ *  // verify navigation to "no match" route
+ *   expect(
+ *    screen.getByText(/<some text>/i)
+ *  ).toBeInTheDocument();
+ */
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
-)
+);
